feaa387c191a418dbf0cc7faa5c32bd7dab11d2d
TedChueh <cch7d5.ee13@nycu.edu.tw>
2026-02-25 02:00:18 +0800

    Fix comment formatting for getCorrectionAimingAngleByVelocity function in math_utils.h

--- src/main/include/utils/math_utils.h
+++ src/main/include/utils/math_utils.h
@@ -18,6 +18,6 @@ using TPS = units::turns_per_second_t;
 Rotation2d getAngleFromRobotToTarget(Translation2d targetPosition, Translation2d robotPosition, Rotation2d robotDirection);
 Translation2d getDeltaTranslation(Translation2d targetPosition, Translation2d referencePosition);
 TPS getTPSFromDistance(double distance, double y_intercept, double slope);
-// Rotation2d getCorrectionAimAngleByVelocity(Rotation2d shootAngle, Translation2d targetPosition, Translation2d robotPosition, ChassisSpeeds robotVelocity);
+// Rotation2d getCorrectionAimingAngleByVelocity(double shootAngle, double deltaHeight, Translation2d targetPosition, Translation2d robotPosition, ChassisSpeeds robotVelocity);
 
 #endif 
